13b555a87bb9a75a53a1ccb403871c18e3e58368
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-12-07 13:56:21 -0800

    Update AT_content.py

--- Conditionals/AT_content.py
+++ Conditionals/AT_content.py
@@ -11,11 +11,11 @@ all_lines = data_file.readlines()
 #This AT_content was one of the original exercises. It has been turned in to a definition to
 #be called upon within the upcoming for loop.
 def AT_content(gene):
-    A_count = gene.count('A')
-    T_count = gene.count('T')
+    A_count = gene.upper().count('A')
+    T_count = gene.upper().count('T')
     Total_length = len(gene)
     #AT content is the percentage of A and T QUANTITY divided by the total length.
-    AT_percentage = 100 * (A_count + T_count)/Total_length
+    AT_percentage = (A_count + T_count)/Total_length
     return AT_percentage
 
 #This for loop cycles through each line in the provided all_lines variable. 
@@ -26,5 +26,11 @@ for line in all_lines:
     #If the AT_content definition for the second item of the split_line list, the gene sequence,
     # is less than 0.5 AND the 4th item of the split_line list, the expression level, is above 200,
     # then the gene name is printed.
+    # An initial run of this program was yielding nothing but 0s as output.
+    # It turns out that my conversion to int was rounding the AT content down to 0.
+    # I used the below print output to diagnose that issue.
+    # print((AT_content(split_line[1])))
+    
     if int(AT_content(split_line[1])) < 0.5 and int(split_line[3]) > 200:
         print ("The gene name is " + (split_line[2]) )
+    
